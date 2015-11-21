52aee5f55e1ee4a3e413ded8c967352f51cd5800
kcreed <kcreed@cox.net>
2015-11-21 02:05:24 -0500

    added src.com.Hello.java

---
+++ src/com/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author Kevin
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
